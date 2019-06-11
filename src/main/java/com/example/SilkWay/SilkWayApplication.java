6b522cf75ab503e0a0b485adf93d2663712fed71
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2019-06-11 09:37:09 +0600

    Initial commit

---
+++ src/main/java/com/example/SilkWay/SilkWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.SilkWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilkWayApplication.class, args);
+	}
+
+}
